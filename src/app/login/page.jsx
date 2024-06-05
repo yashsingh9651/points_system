dda557ef03c2c6f4946c93d85708f92e74a085e6
YASH SINGH <123338008+yashsingh9651@users.noreply.github.com>
2024-06-05 20:59:59 +0530

    Add show/hide password functionality in login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -14,6 +14,7 @@ import { useDispatch } from "react-redux";
 const Page = () => {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
+  const [showPass, setShowPass] = useState(false);
   const dispatch = useDispatch();
   // Form Handling using formik.
   const form = useRef();
@@ -84,9 +85,9 @@ const Page = () => {
                 <Input
                   onBlur={handleBlur}
                   onChange={handleChange}
+                  type={`${!showPass ? "password" : "text"}`}
                   name="password"
                   value={values.password}
-                  type="password"
                   size="lg"
                   placeholder="********"
                   className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
@@ -99,6 +100,21 @@ const Page = () => {
                     {errors.password}
                   </p>
                 )}
+                <svg
+                className="absolute top-2 right-2 cursor-pointer"
+                onClick={() => setShowPass(!showPass)}
+                width="25px"
+                height="25px"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" fill="#0D0D0D" />
+                <path
+                  d="M21.894 11.553C19.736 7.236 15.904 5 12 5c-3.903 0-7.736 2.236-9.894 6.553a1 1 0 0 0 0 .894C4.264 16.764 8.096 19 12 19c3.903 0 7.736-2.236 9.894-6.553a1 1 0 0 0 0-.894zM12 17c-2.969 0-6.002-1.62-7.87-5C5.998 8.62 9.03 7 12 7c2.969 0 6.002 1.62 7.87 5-1.868 3.38-4.901 5-7.87 5z"
+                  fill="#0D0D0D"
+                />
+              </svg>
               </div>
             </div>
             {!loading ? (
